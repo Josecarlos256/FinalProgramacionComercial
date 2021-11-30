5cafe5ecce34c86a7760477379d4dd6fb5e8b3dd
Jose Carlos <josecarlosha@mesoamericana.edu.gt>
2021-11-29 23:16:44 -0600

    Modelos de muchos a muchos con acceso en admins

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from cursos.models import Cursos, CursosAdmin, Estudiantes, EstudiantesAdmin
 # Register your models here.
+
+admin.site.register(Cursos, CursosAdmin)
+admin.site.register(Estudiantes, EstudiantesAdmin)
